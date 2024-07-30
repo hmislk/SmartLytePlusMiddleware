25d7ac84f180c6f2fad69702a4505bfdc9642aa3
Buddhika <buddhika.ari@gmail.com>
2024-07-30 21:39:27 +0530

    Signed-off-by: Buddhika <buddhika.ari@gmail.com>

--- src/main/java/org/carecode/lims/mw/SmartLytePlusMiddleware.java
+++ src/main/java/org/carecode/lims/mw/SmartLytePlusMiddleware.java
@@ -1,15 +1,25 @@
 package org.carecode.lims.mw;
 
+import java.io.FileReader;
+import java.io.IOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSyntaxException;
+
 /**
  *
  * @author Dr M H B Ariyaratne
  */
 public class SmartLytePlusMiddleware {
 
+    private static JsonObject settings;
+
     public static void main(String[] args) {
         System.out.println("MDGPHM!");
-        // Start middleware operations
+
         loadSettings();
+        System.out.println(settings);
         while (true) {
             pingLIMS();
             checkForSamples();
@@ -26,7 +36,12 @@ public class SmartLytePlusMiddleware {
 
     // Load all settings from a JSON file
     public static void loadSettings() {
-        // Method body to be implemented
+        try (FileReader reader = new FileReader("config.json")) {
+            JsonElement jsonElement = JsonParser.parseReader(reader);
+            settings = jsonElement.getAsJsonObject();
+        } catch (IOException | JsonSyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
     // Ping the LIMS intermittently to check for new samples to process
